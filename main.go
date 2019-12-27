297aa7082fdb6993f6d6c8f74d23a550f39f89f0
Luca bockmann <luca.bockmann@8mylez.com>
2019-12-28 00:25:25 +0100

    add General Folder functionality
    - create "General" Folder if it doesn't exist and put file in it
    - put file in Folder "General" if it exist

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	CmdLineArgs := os.Args[1:]
+	FileName := CmdLineArgs[0] + ".txt"
 
 	fmt.Println(CmdLineArgs)
+
+	if len(CmdLineArgs) < 2 {
+		if _, err := os.Stat("./General"); os.IsNotExist(err) {
+			os.Mkdir("General", os.ModePerm)
+			createFileInGeneral(FileName)
+		} else {
+			createFileInGeneral(FileName)
+		}
+	}
+}
+
+func createFileInGeneral(FileName string) {
+	GeneralPath, _ := filepath.Abs("./General/" + FileName)
+	os.Create(GeneralPath)
 }
