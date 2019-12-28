e74608c1be0c631da67c2cbbbd64f8fc46457a07
Luca bockmann <luca.bockmann@8mylez.com>
2019-12-28 02:28:05 +0100

    add custom Folder functionality

--- main.go
+++ main.go
@@ -13,16 +13,42 @@ func main() {
 	fmt.Println(CmdLineArgs)
 
 	if len(CmdLineArgs) < 2 {
-		if _, err := os.Stat("./General"); os.IsNotExist(err) {
-			os.Mkdir("General", os.ModePerm)
-			createFileInGeneral(FileName)
-		} else {
-			createFileInGeneral(FileName)
-		}
+		createGeneral(FileName)
+	} else {
+		FolderName := CmdLineArgs[1]
+		createFolder(FileName, FolderName)
+	}
+}
+
+func createGeneral(FileName string) {
+	if _, err := os.Stat("./General"); os.IsNotExist(err) {
+		fmt.Println("created folder: General")
+		os.Mkdir("General", os.ModePerm)
+		createFileInGeneral(FileName)
+	} else {
+		fmt.Println("add File to Folder: General")
+		createFileInGeneral(FileName)
 	}
 }
 
 func createFileInGeneral(FileName string) {
 	GeneralPath, _ := filepath.Abs("./General/" + FileName)
 	os.Create(GeneralPath)
+}
+
+func createFolder(FileName string, FolderName string) {
+	if _, err := os.Stat(FolderName); os.IsNotExist(err) {
+		fmt.Println("created folder: " + FolderName)
+		os.Mkdir(FolderName, os.ModePerm)
+		createFileInFolder(FileName, FolderName)
+
+	} else {
+		fmt.Println("add File" + FileName + " to Folder:" + FolderName)
+		createFileInFolder(FileName, FolderName)
+	}
+}
+
+func createFileInFolder(FileName string, FolderName string) {
+	FolderPath, _ := filepath.Abs("./" + FolderName + "/" + FileName)
+	os.Create(FolderPath)
 }
